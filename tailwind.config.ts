8634eedacde9cc79bc8a0a4cd667f42dedf0500f
Carlos T <carlosturpo362@gmail.com>
2025-06-22 14:09:05 +0000

    las transiciones se ven muy lentas cuando navego del pane  l central al

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -106,7 +106,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in': 'fade-in 0.5s ease-out forwards',
         'slide-in-left': 'slide-in-left 0.5s ease-out forwards',
-        'content-show': 'content-show 0.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) forwards',
+        'content-show': 'content-show 0.3s ease-out forwards',
         'gradient-pan': 'gradient-pan 15s ease infinite',
         'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
